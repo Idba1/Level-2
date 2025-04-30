01c1db45310d295b1a0bf7e295a9bbdd3e6f8492
Idba1 <moniraislamself@gmail.com>
2025-04-30 22:47:30 +0600

    destructuring in ts

---
+++ TypeScript-TechnoCrat/Module-1/dist/1.8.js
@@ -0,0 +1,23 @@
+"use strict";
+{
+    // destructuring
+    // object destructuring
+    // array destructuring
+    // object destructuring
+    const user = {
+        id: 1017,
+        name: {
+            firstName: 'Monira',
+            middleName: 'Islam',
+            lastName: 'Idba'
+        },
+        contactNo: '01618900000',
+        address: 'Uganda',
+    };
+    const { contactNo: phoneNo, name: { middleName: midName } } = user; //Name Elias
+    console.log(phoneNo, midName);
+    // array destructuring
+    const myFriends = ['Sumaiya', 'Shuva', 'Idba', 'Cara'];
+    const [, , bestFriend, ...rest] = myFriends;
+    console.log(bestFriend);
+}
