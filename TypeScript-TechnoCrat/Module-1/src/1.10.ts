79b0c098d89dde4c38a500392df38afcf67b7521
Idba1 <moniraislamself@gmail.com>
2025-05-01 10:39:29 +0600

    Intersection type

--- TypeScript-TechnoCrat/Module-1/src/1.10.ts
+++ TypeScript-TechnoCrat/Module-1/src/1.10.ts
@@ -1,27 +1,47 @@
 {
 
-    // union types
-    type FrontendDeveloper = 'fakibazDeveloper' | 'juniorDeveloper';
-    type FullStackDeveloper = 'frontendDeveloper' | 'expertDeveloper';
+    // // union types
+    // type FrontendDeveloper = 'fakibazDeveloper' | 'juniorDeveloper';
+    // type FullStackDeveloper = 'frontendDeveloper' | 'expertDeveloper';
 
-    type Developer = FrontendDeveloper | FullStackDeveloper;
+    // type Developer = FrontendDeveloper | FullStackDeveloper;
 
-    const newDeveloper: FrontendDeveloper = 'juniorDeveloper';
+    // const newDeveloper: FrontendDeveloper = 'juniorDeveloper';
 
-    type User = {
-        name: string;
-        email: string;
-        gender: 'male' | 'female';
-        bloodGroup: 'A+' | 'O+' | 'AB+' | 'A-' | 'O-' | 'AB-';
+    // type User = {
+    //     name: string;
+    //     email: string;
+    //     gender: 'male' | 'female';
+    //     bloodGroup: 'A+' | 'O+' | 'AB+' | 'A-' | 'O-' | 'AB-';
+    // }
+
+    // const user: User = {
+    //     name: 'Idba Islam',
+    //     email: 'idba@gmail.com',
+    //     gender: 'female',
+    //     bloodGroup: 'O+',
+    // }
+
+
+
+    // Intersection type
+    type FrontendDeveloper = {
+        skills: string[];
+        designation1: 'Frontend Developer';
     }
 
-    const user: User = {
-        name: 'Idba Islam',
-        email: 'idba@gmail.com',
-        gender: 'female',
-        bloodGroup: 'O+',
+    type BackendDeveloper = {
+        skills: string[];
+        designation2: 'Backend Developer';
     }
 
+    type FullStackDeveloper = FrontendDeveloper & BackendDeveloper;
 
+    const fullstackDeveloper: FullStackDeveloper = {
+        skills: ['html', 'css', 'express', 'dbms'],
+        designation1: 'Frontend Developer',
+        designation2: 'Backend Developer',
+    }
 
+    console.log(fullstackDeveloper.skills);
 }
