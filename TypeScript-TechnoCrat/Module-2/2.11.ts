90e6cfef27f18410873013a0e56aeb1718e77005
Idba1 <moniraislamself@gmail.com>
2025-05-07 19:32:23 +0600

    Utility types

---
+++ TypeScript-TechnoCrat/Module-2/2.11.ts
@@ -0,0 +1,64 @@
+{
+    // utility types
+
+    // pick
+    type Person = {
+        name: string;
+        age: number;
+        email?: string;
+        contactNo: string;
+    }
+
+    type Name = Pick<Person, 'name'>;
+    type NameAge = Pick<Person, 'name' | 'age'>;
+
+
+    // omit
+    type ContactInfo = Omit<Person, 'name' | 'age'>
+
+
+    // Required
+    type PersonRequired = Required<Person>
+
+
+    // Partial
+    type PersonPartial = Partial<Person>
+
+
+    // Readonly
+
+    type PersonReadonly = Readonly<Person>
+
+    const person1: PersonReadonly = {
+        name: 'Rifa Tasniya',
+        age: 9,
+        contactNo: '0199'
+    }
+    // person1.age = 8; //readonly type
+
+
+    // Record
+
+    // type MyObj = {
+    //     a: string;
+    //     b: string
+    // }
+
+    type MyObj = Record<string, string>
+
+    const EmptyObj: Record<string, unknown> = {}
+
+    const obj1: MyObj = {
+        a: 'aa',
+        b: 'bb',
+        c: 'cc',
+        d: 'dd',
+    }
+
+
+
+
+
+
+
+}
