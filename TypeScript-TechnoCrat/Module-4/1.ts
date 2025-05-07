9049095b73646f9d75abbacc59c673e96e275a50
Idba1 <moniraislamself@gmail.com>
2025-05-08 05:41:51 +0600

    problem-1

---
+++ TypeScript-TechnoCrat/Module-4/1.ts
@@ -0,0 +1,16 @@
+{
+    function formatString(input: string, toUpper: boolean = true): string {
+        return toUpper ? input.toUpperCase() : input.toLowerCase();
+    }
+    
+    const test1 = formatString("Hello");
+    console.log(test1);
+    const test2 = formatString("Hello", true);
+    console.log(test2);
+    const test3 = formatString("Hello", false);
+    console.log(test3);
+
+
+
+
+}
