790b4087d39bf07ddd0e242bab5f06368e9ee4f4
Idba1 <moniraislamself@gmail.com>
2025-06-15 19:02:10 +0600

    Asynchronous way to read and write files

--- NodeJS-Fundamentals-L2B5/Module-2/file-system.js
+++ NodeJS-Fundamentals-L2B5/Module-2/file-system.js
@@ -1,15 +1,71 @@
-const fs = require("fs");
+// const fs = require("fs");
+
+// console.log("Task 1");
+
+// const text = "Learning file system"
+
+// fs.writeFileSync("./hello.txt", text)
+
+// console.log("Task 3");
+
+// const data = fs.readFileSync("./hello.txt", { encoding: "utf-8" });
+
+// console.log("Task 4");
+
+// console.log(data);
+
+
+
+
+
+// const fs = require('fs');
+
+// console.log("Task 1");
+
+// let text = "node js";
+// fs.readFile('./hello.txt', { encoding: "utf-8" }, (err, data) => {
+//     if (err) {
+//         console.log("something went wrong", err);
+//         return;
+//     }
+//     text = data;
+//     console.log(text, "Inside call back");
+// });
+
+// console.log(text);
+
+// console.log("Task 3");
+
+
+
+
+
+
+const fs = require('fs');
+
+const data = 'Hey Idba!';
+
+fs.writeFile('output.txt', data, { encoding: "utf-8" }, (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+        return;
+    }
+    console.log('File written successfully!');
+});
+
 
 console.log("Task 1");
 
-const text = "Learning file system"
+let text = "node js";
+fs.readFile('./output.txt', { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+        console.log("something went wrong", err);
+        return;
+    }
+    text = data;
+    console.log(text, "Inside call back");
+});
 
-fs.writeFileSync("./hello.txt", text)
+console.log(text, "from console");
 
 console.log("Task 3");
-
-const data = fs.readFileSync("./hello.txt", { encoding: "utf-8" });
-
-console.log("Task 4");
-
-console.log(data);
