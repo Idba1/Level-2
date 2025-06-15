66cb7caccc24738776b11893d7e8728bd9e795e6
Idba1 <moniraislamself@gmail.com>
2025-06-15 18:35:20 +0600

    Synchronous way to read and write files

---
+++ NodeJS-Fundamentals-L2B5/Module-2/file-system.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+console.log("Task 1");
+
+const text = "Learning file system"
+
+fs.writeFileSync("./hello.txt", text)
+
+console.log("Task 3");
+
+const data = fs.readFileSync("./hello.txt", { encoding: "utf-8" });
+
+console.log("Task 4");
+
+console.log(data);
