afcf925f0d92e02840f2df27c1dd268e58565684
Idba1 <moniraislamself@gmail.com>
2025-06-16 20:10:02 +0600

    u

--- NodeJS-Fundamentals-L2B5/Module-2/file-system.js
+++ NodeJS-Fundamentals-L2B5/Module-2/file-system.js
@@ -41,31 +41,70 @@
 
 
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const data = 'Hey Idba!';
+// const data = 'Hey Idba!';
 
-fs.writeFile('output.txt', data, { encoding: "utf-8" }, (err) => {
-    if (err) {
-        console.error('Error writing file:', err);
-        return;
-    }
-    console.log('File written successfully!');
-});
+// fs.writeFile('output.txt', data, { encoding: "utf-8" }, (err) => {
+//     if (err) {
+//         console.error('Error writing file:', err);
+//         return;
+//     }
+//     console.log('File written successfully!');
+// });
 
 
-console.log("Task 1");
+// console.log("Task 1");
 
-let text = "node js";
-fs.readFile('./output.txt', { encoding: "utf-8" }, (err, data) => {
-    if (err) {
-        console.log("something went wrong", err);
-        return;
-    }
-    text = data;
-    console.log(text, "Inside call back");
-});
+// let text = "node js";
+// fs.readFile('./output.txt', { encoding: "utf-8" }, (err, data) => {
+//     if (err) {
+//         console.log("something went wrong", err);
+//         return;
+//     }
+//     text = data;
+//     console.log(text, "Inside call back");
+// });
 
-console.log(text, "from console");
+// console.log(text, "from console");
 
-console.log("Task 3");
+// console.log("Task 3");
+
+
+
+
+// console.log("Task 1");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// fs.readFile('./hello.txt', { encoding: "utf-8" }, (err, data) => {
+//     if (err) {
+//         console.log("something went wrong", err);
+//         return;
+//     }
+//     fs.writeFile('./output.txt', data, { encoding: "utf-8" }, (err) => {
+//         if (err) {
+//             console.error('Error writing file:', err);
+//             return;
+//         }
+//         console.log('File written successfully!');
+//     });
+// });
+
+
+
---
+++ NodeJS-Fundamentals-L2B5/Module-2/tempCodeRunnerFile.js
@@ -0,0 +1,34 @@
+
+// const fs = require('fs');
+
+// const data = 'Hey Idba!';
+
+// fs.writeFile('output.txt', data, { encoding: "utf-8" }, (err) => {
+//     if (err) {
+//         console.error('Error writing file:', err);
+//         return;
+//     }
+//     console.log('File written successfully!');
+// });
+
+
+// console.log("Task 1");
+
+// let text = "node js";
+// fs.readFile('./output.txt', { encoding: "utf-8" }, (err, data) => {
+//     if (err) {
+//         console.log("something went wrong", err);
+//         return;
+//     }
+//     text = data;
+//     console.log(text, "Inside call back");
+// });
+
+// console.log(text, "from console");
+
+// console.log("Task 3");
+
+
+
+
+// console.log("Task 1");
