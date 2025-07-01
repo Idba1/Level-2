b98fc5185fd878fd6204fb7b96cbd097b537d4f5
Idba1 <moniraislamself@gmail.com>
2025-07-01 09:15:27 +0600

    Handel dark mode using shadcn

--- Redux/Moduler-22/explore-redux/src/components/layout/Navbar.tsx
+++ Redux/Moduler-22/explore-redux/src/components/layout/Navbar.tsx
@@ -1,18 +1,37 @@
 import { Link } from "react-router";
 import logo from "../../assets/react.svg";
+import { ModeToggle } from "../mode-toggler";
 
 export default function Navbar() {
     return (
-        <header className="bg-white shadow fixed top-0 left-0 w-full z-50">
-            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center">
+        <header className="bg-white dark:bg-gray-900 shadow fixed top-0 left-0 w-full z-50">
+            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
+                {/* Logo and App Name */}
                 <div className="flex items-center">
                     <img src={logo} alt="Logo" className="h-8 w-8" />
-                    <span className="ml-2 text-xl font-bold text-gray-800">
+                    <span className="ml-2 text-xl font-bold text-gray-800 dark:text-white">
                         Task<span className="text-blue-600">Master</span>
                     </span>
                 </div>
-                <Link to={"tasks"}>Tasks</Link>
-                <Link to={"users"}>Users</Link>
+
+                {/* Navigation Links */}
+                <div className="flex items-center space-x-6">
+                    <Link
+                        to="/tasks"
+                        className="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
+                    >
+                        Tasks
+                    </Link>
+                    <Link
+                        to="/users"
+                        className="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
+                    >
+                        Users
+                    </Link>
+
+                    {/* Dark Mode Toggle */}
+                    <ModeToggle />
+                </div>
             </nav>
         </header>
     );
